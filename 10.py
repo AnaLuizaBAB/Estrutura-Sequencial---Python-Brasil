da9a98a18d263fdf434d05cc6de87459498b5aeb
AnaLuizaBAB <115850710+AnaLuizaBAB@users.noreply.github.com>
2023-04-19 11:45:50 -0300

    Delete 10.py

--- 10.py
+++
@@ -1,9 +0,0 @@
-# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit
-
-celsius = float(input("\nInforme a temperatura em graus Celsius que pretende transformar em Fahrenheit: "))
-
-fahrenheit = (celsius * 1.8) + 32
-
-print (f"\n{celsius}° Celsius corresponde a {fahrenheit: .2f}° Fahrenheit.")
-
-print("\n")
